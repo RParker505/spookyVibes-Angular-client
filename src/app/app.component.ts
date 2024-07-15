caf425254b875f95263b38365e4b6ad571529806
RParker505 <rchiado75@gmail.com>
2024-07-15 16:42:54 -0600

    Import UserRegistrationFormComponent and write function to open it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
+//This is the app's root component(displays as the homepage)
+
 import { Component } from '@angular/core';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'spookyVibes-Angular-client';
+
+  // Pass MatDialog as an argument so that it's available for use in this component
+  constructor(public dialog: MatDialog) { }
+  
+  // This is the function that will open the dialog when the signup button is clicked  
+  openUserRegistrationDialog(): void {
+      this.dialog.open(UserRegistrationFormComponent, {
+      // Assigning the dialog a width
+      width: '280px'
+      });
+    }
+
 }
