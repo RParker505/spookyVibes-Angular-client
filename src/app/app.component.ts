e8dc50558fbebe5406b9a0c85dc75f43b73eba77
RParker505 <rchiado75@gmail.com>
2024-07-16 06:06:20 -0600

    Update root to open login dialog upon click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@
 
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -18,6 +19,14 @@ export class AppComponent {
   // This is the function that will open the dialog when the signup button is clicked  
   openUserRegistrationDialog(): void {
       this.dialog.open(UserRegistrationFormComponent, {
+      // Assigning the dialog a width
+      width: '280px'
+      });
+    }
+
+  // This is the function that will open the dialog when the login button is clicked  
+  openUserLoginDialog(): void {
+      this.dialog.open(UserLoginFormComponent, {
       // Assigning the dialog a width
       width: '280px'
       });
