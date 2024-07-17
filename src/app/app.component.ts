ccc441b72f7556bb0e1c0250a2b94b33179fe5e4
RParker505 <rchiado75@gmail.com>
2024-07-17 05:49:30 -0600

    Add logic to open MovieCard component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -16,7 +17,7 @@ export class AppComponent {
   // Pass MatDialog as an argument so that it's available for use in this component
   constructor(public dialog: MatDialog) { }
   
-  // This is the function that will open the dialog when the signup button is clicked  
+  // This function will open the dialog when the signup button is clicked  
   openUserRegistrationDialog(): void {
       this.dialog.open(UserRegistrationFormComponent, {
       // Assigning the dialog a width
@@ -24,11 +25,17 @@ export class AppComponent {
       });
     }
 
-  // This is the function that will open the dialog when the login button is clicked  
+  // This function will open the dialog when the login button is clicked  
   openUserLoginDialog(): void {
       this.dialog.open(UserLoginFormComponent, {
       // Assigning the dialog a width
       width: '280px'
+      });
+    }
+  // This function will open the list of all movie cards when button is clicks 
+  openMoviesDialog(): void {
+      this.dialog.open(MovieCardComponent, {
+        width: '500px'
       });
     }
 
