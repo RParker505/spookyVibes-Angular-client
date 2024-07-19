c63d66ac6f3e3d441dfb472bf65b16ae8bf6982f
RParker505 <rchiado75@gmail.com>
2024-07-19 17:46:10 -0600

    Add logic for logging out, redirecting to profile view and showing genre

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { MovieDetailsComponent } from '../movie-details/movie-details.component';
 
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
   styleUrls: ['./movie-card.component.scss']
 })
-export class MovieCardComponent {
+export class MovieCardComponent implements OnInit {
   movies: any[] = [];
-  constructor(public fetchApiData: FetchApiDataService) { }
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public router: Router,
+    public dialog: MatDialog
+  ) { }
 
 // Lifecycle hook called when Angular has created the component
 ngOnInit(): void {
@@ -22,5 +29,24 @@ getMovies(): void {
       return this.movies;
     });
   }
+
+logout(): void {
+  this.router.navigate(["welcome"]);
+  localStorage.clear();
+}
+
+redirectProfile(): void {
+  this.router.navigate(["profile"]);
+}
+
+showGenre(movie: any): void {
+  this.dialog.open(MovieDetailsComponent, {
+      data: {
+          title: String(movie.Genre.Name).toUpperCase(),
+          content: movie.Genre.Description
+      },
+      width: "400px"
+  })
+}
 
 }
