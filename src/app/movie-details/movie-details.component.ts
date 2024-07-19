ea24e12569ca8ecb67fe83b2cf8ecf9f40e67016
RParker505 <rchiado75@gmail.com>
2024-07-19 17:39:30 -0600

    Add logic for movie details component

--- src/app/movie-details/movie-details.component.ts
+++ src/app/movie-details/movie-details.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.component.html',
   styleUrls: ['./movie-details.component.scss']
 })
-export class MovieDetailsComponent {
+export class MovieDetailsComponent implements OnInit {
+
+  constructor(@Inject(MAT_DIALOG_DATA)
+      public data: {
+          title: string,
+          content: string
+      },
+      public dialogRef: MatDialogRef<MovieDetailsComponent>
+    ) {}
+
+    ngOnInit(): void {}
+
+    closeMessageBox(): void {
+      this.dialogRef.close();
+    }
 
 }
